17bc78b2570c2c87be55ee1f564b9099dadbfbca
victorbraga <victor.medeiros@contraprova.com.br>
2023-02-28 11:17:37 -0300

    Atualização Rota Base

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import {Router} from 'express';
 const router = Router();
 
 router.get('/',(req,res)=>{
-    res.json({"mensagem":"Retorno no Routes"});
+    res.json({"mensagem":"Retorno no Routes Atualizado"});
 })
 
 export {router}
