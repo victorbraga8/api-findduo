9f97e75a187360910ed8c8bd14365aed20c5082f
victorbraga <victor.medeiros@contraprova.com.br>
2023-02-28 11:10:24 -0300

    Atualização Teste Vercel

--- src/server.ts
+++ src/server.ts
@@ -1,92 +1,92 @@
 import express from 'express';
 import cors from 'cors';
-import {PrismaClient} from '@prisma/client';
+// import {PrismaClient} from '@prisma/client';
 import { converteHora, converteMinuto } from './utils/utils';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const prisma = new PrismaClient({
-  log: ['query']
-});
+// const prisma = new PrismaClient({
+//   log: ['query']
+// });
 
 
 app.get("/", (req,res)=>{
-  res.json({msg:"Rodando"});
+  res.json({msg:"Rodando Atualizado"});
 })
 
-app.get('/games', async (req,res)=>{
-  const games = await prisma.game.findMany({
-    include:{
-      _count:{
-        select:{
-          ads: true,
-        }
-      }
-    }
-  });
-  return res.json(games);
-})
+// app.get('/games', async (req,res)=>{
+//   const games = await prisma.game.findMany({
+//     include:{
+//       _count:{
+//         select:{
+//           ads: true,
+//         }
+//       }
+//     }
+//   });
+//   return res.json(games);
+// })
 
-app.post('/games/:id/ads', async (req,res)=>{
-  const gameId = req.params.id;
-  const body:any = req.body;
-  const ad = await prisma.ad.create({
-    data:{
-      gameId,
-      name: body.name,
-      yearsPlaying: body.yearsPlaying,
-      discord: body.discord,
-      weekDays: body.weekDays.join(','),
-      hourStart: converteHora(body.hourStart),
-      hourEnd: converteHora(body.hourEnd),
-      useVoiceChannel: body.useVoiceChannel,
-    }
-  })
-  return res.status(201).json(ad)
-});
+// app.post('/games/:id/ads', async (req,res)=>{
+//   const gameId = req.params.id;
+//   const body:any = req.body;
+//   const ad = await prisma.ad.create({
+//     data:{
+//       gameId,
+//       name: body.name,
+//       yearsPlaying: body.yearsPlaying,
+//       discord: body.discord,
+//       weekDays: body.weekDays.join(','),
+//       hourStart: converteHora(body.hourStart),
+//       hourEnd: converteHora(body.hourEnd),
+//       useVoiceChannel: body.useVoiceChannel,
+//     }
+//   })
+//   return res.status(201).json(ad)
+// });
 
-app.get('/games/:id/ads', async (req,res)=>{
-  const gameId = req.params.id;
-  const ads = await prisma.ad.findMany({
-    select:{
-      id:true,
-      name:true,
-      weekDays: true,
-      useVoiceChannel: true,
-      yearsPlaying: true,
-      hourStart: true,
-      hourEnd: true,
-    },
-    where:{
-      gameId: gameId
-    },
-    orderBy:{
-      createdAt: 'desc',
-    }
-  })
-  return res.json(ads.map(ad => {
-    return {
-      ...ad, 
-      weekDays: ad.weekDays.split(','),
-      hourStart: converteMinuto(ad.hourStart),
-      hourEnd: converteMinuto(ad.hourEnd),
-    }
-  }))
-})
+// app.get('/games/:id/ads', async (req,res)=>{
+//   const gameId = req.params.id;
+//   const ads = await prisma.ad.findMany({
+//     select:{
+//       id:true,
+//       name:true,
+//       weekDays: true,
+//       useVoiceChannel: true,
+//       yearsPlaying: true,
+//       hourStart: true,
+//       hourEnd: true,
+//     },
+//     where:{
+//       gameId: gameId
+//     },
+//     orderBy:{
+//       createdAt: 'desc',
+//     }
+//   })
+//   return res.json(ads.map(ad => {
+//     return {
+//       ...ad, 
+//       weekDays: ad.weekDays.split(','),
+//       hourStart: converteMinuto(ad.hourStart),
+//       hourEnd: converteMinuto(ad.hourEnd),
+//     }
+//   }))
+// })
 
-app.get('/ads/:id/discord', async (req,res)=>{
-  const adId = req.params.id;
-  const ad = await prisma.ad.findUniqueOrThrow({
-    select:{
-      discord:true,
-    }, where:{
-      id: adId,
-    }
-  })
-  return res.json({discord: ad.discord})
-})
+// app.get('/ads/:id/discord', async (req,res)=>{
+//   const adId = req.params.id;
+//   const ad = await prisma.ad.findUniqueOrThrow({
+//     select:{
+//       discord:true,
+//     }, where:{
+//       id: adId,
+//     }
+//   })
+//   return res.json({discord: ad.discord})
+// })
 
 const port = process.env.PORT || 3333;
 app.listen(port);
